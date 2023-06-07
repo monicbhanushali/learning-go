9cb6b78ace2dd2d1a13372e06ea618cade2efdf1
Monic Bhanushali <monic.bhanushali@cimpress.com>
2023-06-07 15:08:51 +0530

    Structs example

---
+++ notebook/06_structs/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type person struct {
+	firstname string
+	lastname  string
+}
+
+func main() {
+
+	baburao := person{"Baburao", "Apte"}
+	fmt.Println(baburao)
+
+	raju := person{firstname: "raju", lastname: "chacha"}
+	fmt.Println(raju)
+
+	var shyam person
+	fmt.Println(shyam)
+	fmt.Printf("%+v", shyam)
+}
